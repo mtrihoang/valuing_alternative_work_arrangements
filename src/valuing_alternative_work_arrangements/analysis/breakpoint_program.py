bb28dbb3d4ccffc6c0b968bb7049cc47a493bf70
mtrihoang <trihoang30ec@gmail.com>
2023-03-12 23:45:57 +0100

    Update function breakval_figs in breakpoint_program.py

--- src/valuing_alternative_work_arrangements/analysis/breakpoint_program.py
+++ src/valuing_alternative_work_arrangements/analysis/breakpoint_program.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -6,7 +7,7 @@ from valuing_alternative_work_arrangements.analysis.mle_programs import (
 )
 
 
-def breakval_figs(experimentdata):
+def breakval_figs(df):
     """Search for the optimal breakpoint.
 
     Args:
@@ -17,7 +18,10 @@ def breakval_figs(experimentdata):
         df (pandas.DataFrame): Contain breakpoint predicted values.
 
     """
-    mm1 = me_correction(experimentdata)
+    df = df.groupby(["wagegap"])["chose_position1"].agg(["mean"]).reset_index()
+    df["tag"] = 1
+    df = df.rename(columns={"mean": "chose_position1"})
+    mm1 = me_correction(df)
     mm1["mmk1"] = mm1["mmf1"] - 1
     rmse = []
     K = [
@@ -47,12 +51,8 @@ def breakval_figs(experimentdata):
     ]
     for k in K:
         mm2 = mm1.loc[mm1["wagegap"] >= k, :]
-        model = smf.glm(
-            formula="mmf1 ~ wagegap",
-            data=mm2,
-            family=sm.families.Binomial(),
-        )
-        result = model.fit()
+        model = smf.glm(formula="mmf1 ~ wagegap", data=mm2)
+        result = model.fit(cov_type="HC3")
         mm2["pf"] = result.predict()
         mm3 = mm2[["wagegap", "pf"]]
         mm4 = pd.merge(mm1, mm3, how="left", on="wagegap")
@@ -69,7 +69,7 @@ def breakval_figs(experimentdata):
     break_val = rmse_df.iloc[track].iloc[0, 0]
     mm5 = mm1.loc[mm1["wagegap"] >= break_val, :]
     model = smf.glm(formula="mmf1 ~ wagegap", data=mm5, family=sm.families.Binomial())
-    result = model.fit()
+    result = model.fit(cov_type="HC3")
     mm5["pg"] = result.predict()
     mm6 = mm5[["wagegap", "pg"]]
     df = pd.merge(mm1, mm6, how="left", on="wagegap")
@@ -79,4 +79,6 @@ def breakval_figs(experimentdata):
     b = result.params[1]
     if b > 0:
         df.loc[df["wagegap"] >= break_val, "pg"] = h
+    df["rev_wagegap"] = -df["wagegap"]
+    df = df.sort_values("rev_wagegap")
     return df
