a719e0d6b0d7a8d5d39ea2f42a3fea77a40f8419
mtrihoang <trihoang30ec@gmail.com>
2023-03-13 00:52:21 +0100

    Update function table_1 in 1_main_tables.py

--- src/valuing_alternative_work_arrangements/analysis/1_main_tables.py
+++ src/valuing_alternative_work_arrangements/analysis/1_main_tables.py
@@ -1,5 +1,7 @@
 import itertools
 
+import numpy as np
+import pandas as pd
 import statsmodels.formula.api as smf
 from statsmodels.iolib.summary2 import summary_col
 
@@ -27,24 +29,49 @@ def table_1(df):
         "irreg_inconst",
     ]
     sample = ["", "callcenterworker == 1", "hourly == 1"]
+    dummy = ["C(year)", "C(region)"]
     type = ["", "i.ind1990"]
+    coef_lst = []
     for cond in sample:
         df_ols = df if cond == "" else df.query(cond)
-        for var in xvar:
-            other_var = [
-                var,
-                "uhrswork1",
-                "emp_pt",
-                "female",
-                "age",
-                "selfemp",
-                "foreignborn",
-                "inornearmetro",
-            ]
-            dummy = ["C(year)", "C(region)"]
-            for ind in type:
-                if ind == "i.ind1990":
-                    dummy.append("C(ind1990)")
+        for ind in type:
+            if ind == "i.ind1990":
+                dummy.append("C(ind1990)")
+            for var in xvar:
+                other_var = [
+                    var,
+                    "uhrswork1",
+                    "emp_pt",
+                    "female",
+                    "age",
+                    "selfemp",
+                    "foreignborn",
+                    "inornearmetro",
+                ]
                 formula = "lnearn" + " ~ " + " + ".join(other_var + prefix_var + dummy)
                 res = smf.wls(formula, data=df_ols, weights=df_ols.earnwt).fit()
-                summary_col([res], stars=True).tables[0]
+                coef_table = summary_col([res], stars=True).tables[0]
+                coef_lst.append(coef_table.loc[var, :])
+    coef_matrix = np.array(coef_lst).reshape(6, 6)
+    coef_df = pd.DataFrame(coef_matrix)
+    coef_df = coef_df.rename(
+        index={
+            0: "Can vary the times at which workday starts or ends",
+            1: "Does any work from home",
+            2: "Formal work from home arrangement",
+            3: "Works an irregular schedule",
+            4: "Works an irregular but consistent schedule",
+            5: "Works an irregular, inconsistent schedule",
+        },
+    )
+    coef_df = coef_df.rename(
+        columns={
+            0: "(All) No industry fixed effects",
+            1: "(All) Industry fixed effects",
+            2: "(Phone occupations) No industry fixed effects",
+            3: "(Phone occupations) Industry fixed effects",
+            4: "(All hourly workers) No industry fixed effects",
+            5: "(All hourly workers) Industry fixed effects",
+        },
+    )
+    return coef_df
