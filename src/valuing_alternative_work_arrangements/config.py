3113246a48b93acc0005e9d70c5078c5fb48aaf6
mtrihoang <trihoang30ec@gmail.com>
2023-03-19 23:29:46 +0100

    Update config.py

--- src/valuing_alternative_work_arrangements/config.py
+++ src/valuing_alternative_work_arrangements/config.py
@@ -7,6 +7,8 @@ BLD = SRC.joinpath("..", "..", "bld").resolve()
 TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
 PAPER_DIR = SRC.joinpath("..", "..", "paper").resolve()
 
-TASK_FIGURES = [1, 4, 3, 5, 2]
+TASK_FIGURES_1 = [1, 4, 3, 5, 2]
+
+TASK_FIGURES_2 = ["1a", "4a", "3a", "5a", "2a"]
 
 __all__ = ["BLD", "SRC", "TEST_DIR", "GROUPS"]
